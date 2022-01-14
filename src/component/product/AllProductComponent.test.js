1659f0c62622785611ef12c8658a7ce70cc975df
Khuong <lieukhuong@gmail.com>
2022-01-14 13:26:16 -0500

    try out testing component with jest snapshot v2

--- src/component/product/AllProductComponent.test.js
+++ src/component/product/AllProductComponent.test.js
@@ -22,35 +22,55 @@ import axios from "axios"
 
 
 //https://stackoverflow.com/questions/53545881/how-to-take-a-jest-snapshot-after-axios-fetched-data-in-componentdidmount
-jest.mock("axios")
-const slides = sampleApiResponse()
-const mockedAxiosGet = new Promise(() => ({ data: slides }))
-axios.get.mockImplementation(() => mockedAxiosGet)
+// jest.mock("axios")
+// const slides = sampleApiResponse()
+// const mockedAxiosGet = new Promise(() => ({ data: slides }))
+// axios.get.mockImplementation(() => mockedAxiosGet)
+//
+// it("returns null initally", () => {
+//     const tree = renderer.create(<AllProductComponent />).toJSON()
+//     expect(tree).toMatchSnapshot()
+// })
+//
+// it("uses fetched data to render AllProductComponent", () => {
+//     const tree = renderer.create(<AllProductComponent />)
+//     mockedAxiosGet.then(() => {
+//         expect(tree.toJSON()).toMatchSnapshot()
+//     })
+// })
 
-it("returns null initally", () => {
+// function sampleApiResponse() {
+//     return [
+//
+//         {
+//             productId: 2,
+//             productName: "pd name1",
+//             productDescription: "product descr dummy1",
+//             productPrice: 23,
+//             image: "https://i5.walmartimages.com/asr/b15a04a7-1630-4bc7-8df1-e537e1511150_1.aebae44f8ef9502922894071ae1dabf9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+//         }
+//         ]
+// }
+
+const slides = [
+    {
+        productId: 2,
+             productName: "p",
+             productDescription: "product descr dummy1",
+             productPrice: 3,
+             image: "https://i5.walmartimages.com/asr/b15a04a7-1630-4bc7-8df1-e537e1511150_1.aebae44f8ef9502922894071ae1dabf9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
+    }
+
+]
+
+jest.mock("axios")
+
+it("", async () => {
+    axios.get.mockImplementationOnce(() => Promise.resolve({ data: slides }))
+
     const tree = renderer.create(<AllProductComponent />).toJSON()
+    await Promise.resolve();
     expect(tree).toMatchSnapshot()
 })
-
-it("uses fetched data to render AllProductComponent", () => {
-    const tree = renderer.create(<AllProductComponent />)
-    mockedAxiosGet.then(() => {
-        expect(tree.toJSON()).toMatchSnapshot()
-    })
-})
-
-function sampleApiResponse() {
-    return [
-
-        {
-            productId: 2,
-            productName: "pd name1",
-            productDescription: "product descr dummy1",
-            productPrice: 23,
-            image: "https://i5.walmartimages.com/asr/b15a04a7-1630-4bc7-8df1-e537e1511150_1.aebae44f8ef9502922894071ae1dabf9.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF"
-        }
-        ]
-}
-
 
 
